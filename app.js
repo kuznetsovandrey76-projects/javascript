530c54abf89b107c7830d155b5494719fd8903eb
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-09-11 09:49:25 +0300

    update app.js

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
-// TDD -  test driven development
-// Дизайн кода определяется тестами
-
-// import  Person  from './person';
 var Person = require('./person');
-var express = require('express');
-	app = express();
 
-app.get('/', function (req, res) {
-  res.send(Person.greeting());
-});
-
-app.listen(process.env.PORT || 8080, function(){
-	console.log('http://localhost:8080');
-});
+console.log(Person.greeting())
 
 
 
