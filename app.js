725d38acc50272ca2f23839d100a91d33db15b64
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-09-11 13:59:06 +0300

    factory and constructor

--- app.js
+++ app.js
@@ -1,22 +1,64 @@
 // Module 
-var Counter = (function() {
-	var i = 0;
+// var Counter = (function() {
+// 	var i = 0;
 
+// 	return {
+// 		add: function() {
+// 			i++;
+// 		},
+// 		set: function (value) {
+// 			i = value;
+// 		},
+// 		get: function() {
+// 			console.log(i);
+// 		}
+// 	}
+
+// })();
+
+// Counter.get();
+
+
+
+// prototype / только с new
+// function Person(name, age) {
+// 	this.name = name;
+// 	this.age = age;
+// }
+
+// Person.prototype.greeting = function(){
+// 	return 'Hello ' + this.name; 
+// };
+
+// var andrey = new Person('Andrey', 28);
+// var tom = new Person('Tom', 21);
+
+// console.log(andrey.greeting())
+
+
+
+// Factory Function
+function createObject(value) {
 	return {
-		add: function() {
-			i++;
-		},
-		set: function (value) {
-			i = value;
-		},
-		get: function() {
-			console.log(i);
+		name: value,
+		print: function() {
+			console.log('print');
 		}
+	};
+}
+var obj1 = createObject('string');
+
+// Constructor Function
+function Create(value) {
+	this.name = value;
+	this.print = function() {
+			console.log('print');		
 	}
+}
+var obj2 = new Create('string');
 
-})();
+console.log(obj1, obj2);
 
-Counter.get();
 
 
 
