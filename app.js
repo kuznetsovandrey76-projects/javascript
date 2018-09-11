e90f98d6544ba2949251aa5564e26cf74c56581a
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-09-11 09:13:08 +0300

    add package.json

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+// TDD -  test driven development
+// Дизайн кода определяется тестами
+
+// import  Person  from './person';
+
 var Person = require('./person');
 
 console.log(Person.greeting());
+
+
