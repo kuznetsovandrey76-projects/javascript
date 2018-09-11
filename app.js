f20e726ec791ea65d35507da1b253a7a283a2f4e
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-09-11 09:25:31 +0300

    add express

--- app.js
+++ app.js
@@ -2,9 +2,20 @@
 // Дизайн кода определяется тестами
 
 // import  Person  from './person';
-
 var Person = require('./person');
+var express = require('express');
+	app = express();
 
-console.log(Person.greeting());
+app.get('/', function (req, res) {
+  res.send(Person.greeting());
+});
+
+app.listen(process.env.PORT || 8080, function(){
+	console.log('http://localhost:8080');
+});
+
+
+
+
 
 
