e40c851e752469644d73f4bdf33a0113b8605f36
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-09-12 09:02:09 +0300

    add createElement

--- app.js
+++ app.js
@@ -38,29 +38,49 @@
 
 
 // Factory Function
-function createObject(value) {
-	return {
-		name: value,
-		print: function() {
-			console.log('print');
-		}
-	};
-}
-var obj1 = createObject('string');
+// function createObject(value) {
+// 	return {
+// 		name: value,
+// 		print: function() {
+// 			console.log('print');
+// 		}
+// 	};
+// }
+// var obj1 = createObject('string');
+
+
 
 // Constructor Function
-function Create(value) {
-	this.name = value;
-	this.print = function() {
-			console.log('print');		
-	}
+// function Create(value) {
+// 	this.name = value;
+// 	this.print = function() {
+// 			console.log('print');		
+// 	}
+// }
+// var obj2 = new Create('string');
+
+// console.log(obj1, obj2);
+
+
+
+// var counter = function() {
+// 	var count = 0;
+// 	return function() {
+// 		return ++count;
+//     }
+// };
+
+// var c1 = counter();
+// c1() // 1
+
+
+
+var make = {};
+var tags = ['div', 'p', 'span', 'a'];
+for (var i = 0; i < tags.length; i++) {
+	(function(tag) {
+		make[tag] = function() {
+			return document.createElement(tag);
+		}
+	})(tags[i]);
 }
-var obj2 = new Create('string');
-
-console.log(obj1, obj2);
-
-
-
-
-
-
